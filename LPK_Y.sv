ef7161ff0b878de827f5d26a8c582bba8b5ed0ca
Nino Kristesiashvili <Nino Kristesiashvili>
2022-11-04 21:29:00 +0400

    added 475 hw files

---
+++ LPK_Y.sv
@@ -0,0 +1,15 @@
+module LPK_Y(
+	input [3:0] ARd,
+	input select,
+	output reg [3:0] out
+);
+
+always_comb begin
+	case(select)
+		0: 		out = ARd;
+		1: 		out = 15;
+		default: out = 0;
+	endcase
+end
+
+endmodule
